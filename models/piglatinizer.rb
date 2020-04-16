25dc6c6d0bdbf1f76909c45f2182be3de0fa1be7
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-16 17:06:02 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,7 +5,7 @@ class PigLatinizer
     input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
 
-  private
+  #private
 
   def consonant?(char)
     !char.match(/[aAeEiIoOuU]/)
